3d468051f6e6750dcb9427da7cfd258bb33b1596
Andre Ziviani <andrepziviani@gmail.com>
2023-10-25 02:34:32 -0300

    fix: Dont exit when ESC is pressed

--- internal/target/tui.go
+++ internal/target/tui.go
@@ -90,7 +90,9 @@ func (m mainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c":
+		case "esc":
+			return m, nil
+		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "tab":
 			if m.state == targetsView {
